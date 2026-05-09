ba47b698c831a3f9dc5300c8708af37984e0aa64
Wladyslaw Rouba <roubovlad@gmail.com>
2026-05-10 00:50:04 +0200

    Add solution - CodeHub

---
+++ 6-kyu/roman-numerals-decoder/roman-numerals-decoder.js
@@ -0,0 +1,25 @@
+ function solution (roman) {
+  // complete the solution by transforming the 
+  // string roman numeral into an integer
+   const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+   let result = 0
+   for(let i = 0; i < roman.length; i++ ){
+     const current = map[roman[i]]
+     const next = map[roman[i + 1]]
+                      if(next > current){
+                        result -= current
+                      }else{
+                        result += current
+                      }
+                      
+   }
+  return result;
+}
